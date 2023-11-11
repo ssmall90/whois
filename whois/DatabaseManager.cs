1df703226f2fbd02a557a3e52bc99b9b8416f9bd
Gin-Sweets <106976323+ssmall90@users.noreply.github.com>
2023-11-11 12:43:56 +0000

    Add project files.

---
+++ whois/DatabaseManager.cs
@@ -0,0 +1,143 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whois
+{
+    internal class DatabaseManager
+    {
+        static string connectionString = "server=localhost;user=root;database=acw_whois_database;port=3306;password=L3tM31n";
+        private MySqlConnection _connection;
+
+
+        public MySqlConnection Connection { get { return _connection; } }
+
+        public DatabaseManager()
+        {
+            _connection = new MySqlConnection(connectionString);
+        }
+
+        public void GetDump(string LoginId)
+        {
+
+            _connection.Open();
+
+            string getUserDump = $"SELECT * FROM acw_whois_database.users WHERE LoginId = '{LoginId}'";
+
+            MySqlCommand cmd = new MySqlCommand(getUserDump, _connection);
+
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine($"User Id: {rdr[0]}");
+                Console.WriteLine($"Login Id: {rdr[1]}");
+                Console.WriteLine($"Title: {rdr[2]}");
+                Console.WriteLine($"Fornames: {rdr[3]}");
+                Console.WriteLine($"Surname: {rdr[4]}");
+                Console.WriteLine($"Position: {rdr[5]}");
+                Console.WriteLine($"Email: {rdr[6]}");
+                Console.WriteLine($"Phone: {rdr[7]}");
+                Console.WriteLine($"Location: {rdr[8]}");
+
+            }
+            rdr.Close();
+
+            _connection.Close();
+
+        }
+
+        public string GetLookup(string LoginId, string field)
+        {
+            _connection.Open();
+
+            string result = null;
+
+            string getLookUp = $"SELECT {field} FROM acw_whois_database.users WHERE loginId = '{LoginId}';";
+
+            MySqlCommand cmd = new MySqlCommand(getLookUp, _connection);
+
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                result = ($"Look up of {field} for {LoginId} returned {rdr[0]}");
+            }
+
+            _connection.Close();
+
+            return result;
+
+        }
+
+        public void UpdateNewUser(string LoginId, string field, string valueToInsert)
+        {
+            _connection.Open();
+
+            int lastID = 0;
+
+            string retrieveLastId = "SELECT MAX(userId) FROM users";
+
+            MySqlCommand cmd = new MySqlCommand(retrieveLastId, _connection);
+
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                lastID = int.Parse(rdr[0].ToString()!) + 1;
+
+            }
+            rdr.Close();
+
+            string addNewUser = $"INSERT INTO acw_whois_database.users (`userId`,`loginId`,`{field}`) VALUES ({lastID},'{LoginId}','{valueToInsert}')";
+
+            cmd.CommandText = addNewUser;
+
+            cmd.ExecuteNonQuery();
+
+            _connection.Close();
+
+
+        }
+
+        public string UpdateExistingUser(string LoginId, string field, string valueToInsert)
+        {
+
+            try
+            {
+                //_connection.Open();
+
+                if (GetLookup(LoginId,field) != null)
+                {
+                    _connection.Open();
+
+                    string UpdateUserInfo = $"UPDATE acw_whois_database.users SET {field} = '{valueToInsert}' WHERE loginId = '{LoginId}'";
+
+                    MySqlCommand cmd = new MySqlCommand(UpdateUserInfo, _connection);
+
+                    cmd.ExecuteNonQuery();
+
+                    _connection.Close();
+
+                    return $"Successfully Updated {LoginId}'s {field} to {valueToInsert}";
+                }
+
+                else
+                { 
+                    return $"{LoginId} could not be found in database";
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                return $"Database Error {(ex.ToString())}";
+            }
+
+        }
+
+    }
+}
---
+++ whois/Program.cs
@@ -0,0 +1,284 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using whois;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
+
+DatabaseManager databaseManager = new DatabaseManager();
+
+if (args.Length == 0)
+{
+    RunServer();
+}
+else
+{
+    for (int i = 0; i < args.Length; i++)
+    {
+        ProcessCommand(args[i]);
+    }
+}
+
+
+void RunServer()
+{
+
+    TcpListener listener;
+    Socket connection;
+    NetworkStream socketStream;
+    try
+    {
+        listener = new TcpListener(IPAddress.Any, 43);
+        listener.Start();
+        Console.WriteLine("Server Has Started Listening");
+
+        while (true)
+        {
+            connection = listener.AcceptSocket();
+            socketStream = new NetworkStream(connection);
+            connection.SendTimeout = 10000;
+            connection.ReceiveTimeout = 10000;
+            Console.WriteLine("Connection Received");
+            DoRequest(socketStream);
+            socketStream.Close();
+            connection.Close();
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.ToString()); // An example
+    }
+}
+
+void DoRequest(NetworkStream socketStream)
+{
+
+    StreamWriter sw = new StreamWriter(socketStream);
+    StreamReader sr = new StreamReader(socketStream);
+
+    try
+    {
+
+        string line = sr.ReadLine();
+
+
+        if (line == null)
+        {
+            Console.WriteLine("Ignoring null command");
+            return;
+        }
+
+        else if (line == "POST / HTTP/1.1")
+        {
+            int contentLength = 0;
+
+            while (line != "")
+            {
+                line = sr.ReadLine(); // Skip to blank line
+
+                if (line.StartsWith("Content-Length: "))
+                {
+                    contentLength = Int32.Parse(line.Substring(16));
+                }
+            }
+
+            line = "";
+
+            for (int i = 0; i < contentLength; i++) line += (char)sr.Read();
+
+            String[] slices = line.Split(new char[] { '&' }, 2);
+            String ID = slices[0].Substring(5);
+            String value = slices[1].Substring(9);
+
+            string result = databaseManager.UpdateExistingUser(ID, "location", value);
+
+            if (result.Contains("could not be found in database"))
+            {
+                sw.WriteLine("HTTP/1.1 404 Not Found");
+                sw.WriteLine("Content-Type: text/plain");
+                sw.WriteLine();
+                sw.WriteLine(result);
+                sw.Flush();
+                Console.WriteLine($"Received an update request for '{ID}' to '{value}");
+                Console.WriteLine(result);
+            }
+            else
+            {
+
+                sw.WriteLine("HTTP/1.1 200 OK");
+                sw.WriteLine("Content-Type: text/plain");
+                sw.WriteLine();
+                sw.WriteLine(result);
+                sw.Flush();
+
+                Console.WriteLine($"Received an update request for '{ID}' to '{value}");
+                Console.WriteLine(result);
+            }
+
+        }
+
+        else if (line.StartsWith("GET /?name=") && line.EndsWith(" HTTP/1.1"))
+        {
+            string[] slices = line.Split(" ");  // Split into 3 pieces
+            string ID = slices[1].Substring(7);  //
+            string result = (databaseManager.GetLookup(ID, "location"));
+
+            if (result is not null)
+            {
+
+                sw.WriteLine("HTTP/1.1 200 OK");
+                sw.WriteLine("Content-Type: text/plain");
+                sw.WriteLine();
+                sw.WriteLine(result);
+                sw.Flush();
+
+                Console.WriteLine($"Performed Lookup on '{ID}' returning '{result}'");
+            }
+
+            else
+            {
+                sw.WriteLine("HTTP/1.1 404 Not Found");
+                sw.WriteLine("Content-Type: text/plain");
+                sw.WriteLine();
+                sw.Flush();
+                Console.WriteLine($"Performed Lookup on '{ID}' returning '404 Not Found'");
+
+            }
+
+        }
+        else
+        {
+
+            sw.WriteLine("HTTP/1.1 400 Bad Request");
+            sw.WriteLine("Content-Type: text/plain");
+            sw.WriteLine();
+            sw.Flush();
+
+            Console.WriteLine($"Unrecognised command: '{line}'");
+
+        }
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Fault in Command Processing:{ex.ToString()} ");
+        sw.Close();
+        sr.Close();
+    }
+    finally
+    {
+        sw.Close();
+        sr.Close();
+
+    }
+}
+
+void ProcessCommand(string command)
+{
+    DatabaseManager databaseManager = new DatabaseManager();
+
+    try
+    {
+        Console.WriteLine($"\nCommand: {command}");
+        String[] slice = command.Split(new char[] { '?' }, 2);
+        String ID = slice[0];
+        String operation = null;
+        String update = null;
+        String field = null;
+
+        if (slice.Length == 2)
+        {
+            operation = slice[1];
+            String[] pieces = operation.Split(new char[] { '=' }, 2);
+            field = pieces[0];
+            if (pieces.Length == 2) update = pieces[1];
+        }
+
+        if (operation == null || operation == string.Empty)
+        {
+
+            if (databaseManager.GetLookup(ID, "loginId") is not null)
+            {
+                Dump(ID);
+            }
+            else
+            {
+                Console.WriteLine("User does not exist");
+            }
+        }
+
+        else if (update == null)
+        {
+            Lookup(ID, field);
+        }
+
+        else
+        {
+            if (databaseManager.GetLookup(ID, "loginId") is not null)
+            {
+                string newID = command.Split("?")[0];
+
+                update = command.Split("=")[1];
+
+                databaseManager.UpdateNewUser(newID, field, update);
+
+            }
+            else
+            {
+                update = command.Split("=")[1];
+                Update(ID, field, update);
+            }
+
+        }
+    }
+
+    catch (Exception e)
+    {
+        Console.WriteLine($"Fault in Command Processing: {e.ToString()}");
+    }
+
+}
+
+void Dump(String ID)
+{
+
+    databaseManager.GetDump(ID);
+
+
+}
+
+void Lookup(String ID, String field)
+{
+    if (databaseManager.GetLookup(ID, "loginId") is not null)
+    {
+        Console.WriteLine($"lookup field: {field}");
+        Console.WriteLine(databaseManager.GetLookup(ID, field));
+    }
+    else
+    {
+        Console.WriteLine($"lookup field: {field}");
+        Console.WriteLine($"User '{ID}' does not exist");
+    }
+
+}
+
+void Update(String ID, String field, String update)
+{
+
+    if (databaseManager.GetLookup(ID, "loginId") is not null)
+    {
+
+        databaseManager.UpdateExistingUser(ID, field, update);
+
+        Console.WriteLine(databaseManager.GetLookup(ID, field));
+    }
+    else
+    {
+        databaseManager.UpdateNewUser(ID, field, update);
+    }
+
+
+    Console.WriteLine("OK");
+}
+
